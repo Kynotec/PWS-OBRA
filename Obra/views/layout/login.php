f93752b5737bee0dcefd5e53a232b79cf4cfe609
Kynotec <2211867@my.ipleiria.pt>
2023-05-26 10:42:55 +0100

    login layout

--- Obra/views/layout/login.php
+++ Obra/views/layout/login.php
@@ -4,14 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= APP_NAME ?> | Iniciar Sessão</title>
-    <link rel="icon" href="./public/dist/img/FaturaMaisLogo.png">
+    <link rel="icon" href="./public/assets/img/FaturaMaisLogo.png">
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="./public/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="./public/assets/plugins/fontawesome-free/css/all.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="./public/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="./public/assets/css/adminlte.min.css">
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
@@ -63,10 +63,10 @@
 <!-- /.login-box -->
 
 <!-- jQuery -->
-<script src="./public/plugins/jquery/jquery.min.js"></script>
+<script src="./public/assets/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="./public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="./public/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="./public/dist/js/adminlte.min.js"></script>
+<script src="./public/assets/js/adminlte.min.js"></script>
 </body>
 </html>
