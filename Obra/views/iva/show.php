7d396a06fe8f116427f8bc62f48f1044c44b75e7
Kynotec <2211867@my.ipleiria.pt>
2023-06-05 21:32:29 +0100

    update iva show

--- Obra/views/iva/show.php
+++ Obra/views/iva/show.php
@@ -12,7 +12,7 @@
     <section class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-md-8">
+                <div class="col-md-10">
                     <div class="card card-primary">
                         <div class="card-header">
                             <h3 class="card-title ">Dados do Iva</h3>
