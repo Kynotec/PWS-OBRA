91922754dba5f0031522c391db228f2e3531ca57
Kynotec <2211867@my.ipleiria.pt>
2023-06-18 01:02:18 +0100

    update layout fo

--- Obra/views/home/index.php
+++ Obra/views/home/index.php
@@ -1,59 +1,61 @@
 <!-- banner section start -->
 <div class="banner_section">
     <div class="container-fluid">
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="row">
-                        <div class="col-sm-2">
-                            <div class="round"><img src="./public/dist/img/round-icon.png"></div>
-                            <div class="line"><img src="./public/dist/img/line.png"></div>
-                            <div class="line"><img src="./public/dist/img/line.png"></div>
-                            <div class="line"><img src="./public/dist/img/line.png"></div>
-                            <div class="round"><img src="./public/dist/img/round-icon.png"></div>
-                        </div>
-                        <div class="col-sm-5">
-                            <div class="taital_main">
-                                <h2 class="construction_text">CONSTRUCTION</h2>
-                                <h1 class="building_text">BUILDING</h1>
-                                <p class="lorem_text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure </p>
-                                <div class="contact_bt"><a href="https://www.ipleiria.pt/estg/escola/contactos/">Contactos</a></div>
-                            </div>
-                        </div>
-                        <div class="col-sm-5">
-                            <div><img src="./public/dist/img/banner-bg.png"></div>
-                        </div>
-                    </div>
+        <div class="container">
+
+        </div>
 
 </div>
 <!-- banner section end -->
 <!-- our service section start -->
 <div class="our_section layout_padding">
-    <div class="container">
-        <h1 class="our_text"><strong>Os nossos Serviços</strong></h1>
-        <p class="client_long_text"></p>
-        <div class="row padding_top_0">
-            <div class="col-lg-4">
-                <div class="image_7"><a href="#"><img src="./public/dist/img/img-1.png"></a></div>
-                <h2 class="design_text">Gestão de Folhas de Obras</h2>
-                <p class="fact_text">Com o nosso serviço nunca foi táo fácil criar uma folha de Obra!</p>
-            </div>
-            <div class="col-lg-4">
-                <div class="image_7"><a href="#"><img src="./public/dist/img/img-2.png"></a></div>
-                <h2 class="design_text">Gestão de Utilizadores</h2>
-                <p class="fact_text">Pode ver as suas folhas de Obra a qualquer altura entre já com as suas credencias!</p>
-            </div>
-            <div class="col-lg-4">
-                <div class="image_7"><a href="#"><img src="./public/dist/img/img-3.png"></a></div>
-                <h2 class="design_text">Sistema Simples</h2>
-                <p class="fact_text">Plataforma simples e com um visual apelativo!</p>
-            </div>
-            <div class="bt_main">
 
-            </div>
-        </div>
-    </div>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <h1 class="our_text"><strong>Os nossos Serviços</strong></h1>
+                <div class="row">
+
+                    <div class="col-sm-2">
+
+                    </div>
+                    <div class="col-sm-5">
+                        <div class="taital_main">
+
+                        </div>
+                        <div><img src="./public/dist/img/banner-bg.png" class="image"></div>
+                    </div>
+                    <div class="col-sm-5">
+
+                        <p class="client_long_text"></p>
+                        <div class="row padding_top_0">
+                            <div class="col-lg-4">
+                                <div class="image_7"><a href="#"><img src="./public/dist/img/img-1.png"></a></div>
+                                <h2 class="design_text">Gestão de Folhas de Obras</h2>
+                                <p class="fact_text">Com o nosso serviço nunca foi táo fácil criar uma folha de Obra!</p>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="image_7"><a href="#"><img src="./public/dist/img/img-2.png"></a></div>
+                                <h2 class="design_text">Gestão de Utilizadores</h2>
+                                <p class="fact_text">Pode ver as suas folhas de Obra a qualquer altura entre já com as suas credencias!</p>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="image_7"><a href="#"><img src="./public/dist/img/img-3.png"></a></div>
+                                <h2 class="design_text">Sistema Simples</h2>
+                                <p class="fact_text">Plataforma simples e com um visual apelativo!</p>
+                            </div>
+
+                        </div>
+
+
+                    </div>
+                </div>
+
+                <style>
+                    .image {
+                        float: left; /* ou float: right; */
+                        margin-left: -300px; /* ajuste a margem conforme necessário */
+                    }
+                </style>
 </div>
 <!-- our service section end -->
-
-
--- Obra/views/layout/obra.php
+++ Obra/views/layout/obra.php
@@ -94,6 +94,5 @@
 <!-- javascript -->
 <script src="./public/dist/js/owl.carousel.js"></script>
 <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
-
 </body>
 </html>
