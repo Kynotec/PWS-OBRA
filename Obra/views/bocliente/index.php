6e6bb6ad595bea6bd5c8924fdfaec639a0989d78
Kynotec <2211867@my.ipleiria.pt>
2023-06-20 11:41:15 +0100

    update bocliente

--- Obra/views/bocliente/index.php
+++ Obra/views/bocliente/index.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0">PATO ÉS G@Y</h1>
+                <h1 class="m-0"></h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
